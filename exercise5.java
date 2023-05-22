b89ec9b011a14620a9f807d6b1dc9fc6db7fe6be
Fernando Franco <francoduranluisfernando@gmail.com>
2023-05-22 17:56:23 -0600

    Celsius a Fahrenheit

---
+++ exercise5.java
@@ -0,0 +1,12 @@
+package javaintroexercises;
+
+import java.util.Scanner;
+
+public class exercise5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero entero: ");
+        int
+    }
+}
