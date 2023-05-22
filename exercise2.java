cc35876b9ad645d06805905d8d87364ec41899ad
Fernando Franco <francoduranluisfernando@gmail.com>
2023-05-22 17:37:58 -0600

    Guardar nombre y mostrar

---
+++ exercise2.java
@@ -0,0 +1,14 @@
+package javaintroexercises;
+
+import java.util.Scanner;
+
+public class exercise2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre: ");
+        String name = sc.nextLine();
+
+        System.out.println("Su nombre es: " + name);
+    }
+}
