8e1e51f6b5b16d84c5dd2b1405d7e2f7cd7c00d8
Fernando Franco <francoduranluisfernando@gmail.com>
2023-05-23 16:39:45 -0600

    .equals(eureka)

---
+++ exercise7.java
@@ -0,0 +1,17 @@
+package javaintroexercises;
+
+import java.util.Scanner;
+
+public class exercise7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String phrase = "";
+
+        //Necesitas el ! porque el while debe tener TRUE para comenzarlo
+        
+        while (!phrase.equalsIgnoreCase("eureka")) {
+            System.out.println("Ingrese una frase: ");
+            phrase = sc.nextLine();
+        }
+    }
+}
