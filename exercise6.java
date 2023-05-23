929a02379ded7b4545c73ddae8800cfb2ad1efcc
Fernando Franco <francoduranluisfernando@gmail.com>
2023-05-23 16:31:52 -0600

    Número par o impar

---
+++ exercise6.java
@@ -0,0 +1,20 @@
+package javaintroexercises;
+
+import java.util.Scanner;
+
+public class exercise6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero entero: ");
+        int num = sc.nextInt();
+
+        if (num == 0) {
+            System.out.println("El numero es par");
+        } else if ((num % 2) == 0) {
+            System.out.println("El numero es par");
+        } else {
+            System.out.println("El numero es impar");
+        }
+    }
+}
