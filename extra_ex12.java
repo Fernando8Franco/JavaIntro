ead934932a9b4a78a04ff26f252d0daf528df9c4
Fernando Franco <francoduranluisfernando@gmail.com>
2023-05-30 17:37:14 -0600

    Imprimir números con formato

---
+++ extra_ex12.java
@@ -0,0 +1,15 @@
+package javaintroexercises;
+
+import java.text.DecimalFormat;
+
+public class extra_ex12 {
+    public static void main(String[] args) {
+        DecimalFormat format_num = new DecimalFormat("000");
+        String num_format;
+
+        for (int i = 0; i < 1000; i++) {
+            num_format = format_num.format(i);
+            System.out.printf("%s-%s-%s%n", num_format.charAt(0), num_format.charAt(1), num_format.charAt(2));
+        }
+    }
+}
