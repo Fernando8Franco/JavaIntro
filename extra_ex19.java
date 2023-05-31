d0b1d14b2741d23b4bfa169abb44e6c481837fbe
Fernando Franco <francoduranluisfernando@gmail.com>
2023-05-31 17:15:02 -0600

    Comprobar arreglos iguales

---
+++ extra_ex19.java
@@ -0,0 +1,12 @@
+package javaintroexercises;
+
+import java.util.Arrays;
+
+public class extra_ex19 {
+    public static void main(String[] args) {
+        int[] v1 = {1, 2, 3, 4, 5};
+        int[] v2 = {1, 2, 3, 4, 5};
+
+        System.out.println((Arrays.equals(v1, v2) ? "S" : "NO s") + "on arreglos iguales");
+    }
+}
