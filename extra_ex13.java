a558209490c32cc772ce3d4d33de2090d3631695
Fernando Franco <francoduranluisfernando@gmail.com>
2023-05-30 18:07:31 -0600

    Escalera de números

--- extra_ex13.java
+++ extra_ex13.java
@@ -1,2 +1,18 @@
-package javaintroexercises;public class extra_ex13 {
+package javaintroexercises;
+
+import java.util.Scanner;
+
+public class extra_ex13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String num = "";
+
+        System.out.println("Ingresa el tamaño de la escalera: ");
+        int size = sc.nextInt();
+
+        for (int i = 1; i <= size; i++) {
+            num = num.concat(String.valueOf(i));
+            System.out.println(num);
+        }
+    }
 }
