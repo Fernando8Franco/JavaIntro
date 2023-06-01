f5bc24da4dfffea156b3499ca42cf1502dcc7b2b
Fernando Franco <francoduranluisfernando@gmail.com>
2023-06-01 16:27:36 -0600

    Ingresar nombre y mostrar

--- exercise02.java
+++ exercise02.java
@@ -6,7 +6,7 @@ public class exercise02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrese su nombre: ");
+        System.out.print("Ingrese su nombre: ");
         String name = sc.nextLine();
 
         System.out.println("Su nombre es: " + name);
